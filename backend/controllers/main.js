1d3e94d5d863edf8b122d8c22385fec425567d87
Sumit <sumitsharma9968@gmail.com>
2023-01-08 16:58:16 +0530

    Added about, contact, login, register, forgot-password routes

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,5 +1,25 @@
 // main controller
 
 module.exports.home = function (req, res) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "The MEAN Stack Agency" });
+};
+
+module.exports.about = function (req, res) {
+  res.render("about");
+};
+
+module.exports.contact = function (req, res) {
+  res.render("contact");
+};
+
+module.exports.login = function (req, res) {
+  res.render("login");
+};
+
+module.exports.register = function (req, res) {
+  res.render("register");
+};
+
+module.exports.forgotpassword = function (req, res) {
+  res.render("forgot-password");
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,5 +4,15 @@ const mainController = require("../controllers/main");
 
 /* GET home page. */
 router.get("/", mainController.home);
+/* GET about page. */
+router.get("/about", mainController.about);
+/* GET contact page. */
+router.get("/contact", mainController.contact);
+/* GET login page. */
+router.get("/login", mainController.login);
+/* GET register page. */
+router.get("/register", mainController.register);
+/* GET forgot-password page. */
+router.get("/forgot-password", mainController.forgotpassword);
 
 module.exports = router;
