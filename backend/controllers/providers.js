003c029ed3de0b4c373b8f69f7564b3883785db4
Sumit <sumitsharma9968@gmail.com>
2023-01-08 19:10:55 +0530

    Added providers details

--- backend/controllers/providers.js
+++ backend/controllers/providers.js
@@ -2,5 +2,19 @@ const providers = require("../models/providers");
 
 // providers controller
 module.exports.list = function (req, res) {
-  res.render("providers/providers-list", { providers: providers });
+  res.render("providers/providers-list", {
+    title: "Service Providers",
+    providers: providers,
+  });
+};
+
+module.exports.details = function (req, res) {
+  let id = req.params.id;
+  let provider = providers.find((provider) => provider.id == id);
+
+  res.render("providers/providers-details", {
+    id: id,
+    title: "Service Provider Details",
+    company: provider.company,
+  });
 };
--- backend/routes/providers.js
+++ backend/routes/providers.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 const providerController = require("../controllers/providers");
 
-/* GET service providers list. */
+/* GET service providers list page. */
 router.get("/", providerController.list);
+/* GET service providers details page. */
+router.get("/details/:id", providerController.details);
 
 module.exports = router;
